3e409e6758e1dc2f080e92de23c1d23b17c53cd8
Nabil080 <bellilanabil8@gmail.com>
2024-05-16 13:23:31 +0200

    isprint proto

--- Makefile
+++ Makefile
@@ -5,6 +5,7 @@ SRCS = 	ft_strlen.c \
 		ft_isdigit.c \
 		ft_isalnum.c \
 		ft_isascii.c \
+		ft_isprint.c \
 
 FLAGS = -Wall -Wextra -Werror
 
--- ft_isprint.c
+++ ft_isprint.c
@@ -1,35 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   header.h                                           :+:      :+:    :+:   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/15 13:58:27 by nbellila          #+#    #+#             */
-/*   Updated: 2024/05/16 13:23:03 by nbellila         ###   ########.fr       */
+/*   Created: 2024/05/16 11:50:33 by nbellila          #+#    #+#             */
+/*   Updated: 2024/05/16 13:20:25 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef HEADER_H
-# define HEADER_H
+#include "header.h"
 
-# include <unistd.h>
-# include <stdlib.h>
-
-size_t	ft_strlen(const char *s);
-
-size_t	ft_strlcpy(char *dst, const char *src, size_t siz);
-
-size_t	ft_strlcat(char *dst, const char *src, size_t siz);
-
-int		ft_isalpha(int c);
-
-int		ft_isdigit(int c);
-
-int		ft_isalnum(int c);
-
-int		ft_isascii(int c);
-
-int		ft_isprint(int c);
-
-#endif
+int	ft_isprint(int c)
+{
+	return ((c >= ' ' && c <= '~'));
+}
