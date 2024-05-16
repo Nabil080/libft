b395dcbeab0b347b7cefec4a9795d2f483a2e9a6
Nabil Bellila <nbellila@bess-f2r3s8.clusters.42paris.fr>
2024-05-16 12:54:15 +0200

    isdigit done + test

--- tester.h
+++ tester.h
@@ -6,14 +6,14 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 15:05:47 by nbellila          #+#    #+#             */
-/*   Updated: 2024/05/16 12:16:44 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/05/16 12:50:50 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TESTER_H
 # define TESTER_H
 
-# define NUMS_SIZE 7
+# define NUMS_SIZE 10
 
 # include <stdio.h>
 # include <string.h>
@@ -22,11 +22,15 @@
 
 char	**test_strings(void);
 
+char	*test_chars(void);
+
 int		*test_nums(void);
 
 void	test_strlen(char **strings, int argc);
 
-void	test_isalpha(int *nums, int argc);
+void	test_isalpha(char *chars, int argc);
+
+void	test_isdigit(char *chars, int argc);
 
 void	test_strlcpy(char **strings, int argc);
 
