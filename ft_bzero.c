b8bb072d1acd27b0e60e89c3e72f6123ec41faa3
Nabil080 <bellilanabil8@gmail.com>
2024-05-18 15:51:36 +0200

    fix strncmp, cmp n - 1

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 16:18:30 by nbellila          #+#    #+#             */
-/*   Updated: 2024/05/18 14:52:28 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/05/18 15:40:06 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@ void	ft_bzero(void *s, size_t n)
 	char	*ptr;
 
 	ptr = (char *) s;
-	while (n--)
+	while (n-- > 0)
 		*ptr++ = 0;
 }
