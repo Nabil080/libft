b3e4fbdccb94498ef724ee9c675761cf046a15c1
Nabil080 <bellilanabil8@gmail.com>
2024-05-18 10:20:05 +0200

    ft_strlcat

--- Makefile
+++ Makefile
@@ -11,6 +11,7 @@ SRCS = 	ft_strlen.c \
 		ft_memcpy.c \
 		ft_memmove.c \
 		ft_strlcpy.c \
+		ft_strlcat.c \
 		ft_atoi.c \
 
 FLAGS = -Wall -Wextra -Werror
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,15 +1,15 @@
 #include "header.h"
 
-size_t  ft_strlcpy(char *dst, const char *src, size_t size)
+size_t  ft_strlcpy(char *dst, const char *src, size_t siz)
 {
     size_t  src_len;
     size_t  dest_len;
 
     src_len = ft_strlen(src);
-    if (size == 0)
+    if (siz == 0)
         return (src_len);
     dest_len = 0;
-    while (src[dest_len] && dest_len < size - 1)
+    while (src[dest_len] && dest_len < siz - 1)
     {
         dest[dest_len] = src[dest_len];
         dest_len++;
