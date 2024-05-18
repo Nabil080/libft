ed34d96dc8aec74f254fc5e91b7b294da4d58bdb
Nabil080 <nbellila@student.42.fr>
2024-05-18 15:51:36 +0200

    fix strncmp, cmp n - 1

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 14:32:04 by nbellila          #+#    #+#             */
-/*   Updated: 2024/05/18 14:52:28 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/05/18 15:51:07 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,16 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	*us1;
+	unsigned char	*us2;
 
 	if (n == 0)
 		return (0);
+	us1 = (unsigned char *) s1;
+	us2 = (unsigned char *) s2;
 	i = 0;
-	while (s1[i] && s2[i] && s1[i] == s2[i] && i < n)
+	while (us1[i] && us2[i] && us1[i] == us2[i] && i < n - 1)
 		i++;
-	return (s1[i] - s2[i]);
+	return (us1[i] - us2[i]);
 }
