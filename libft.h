6085bb6fc3f23f068782253150bdcb119a4cea07
Nabil080 <nbellila@student.42.fr>
2024-05-18 18:55:24 +0200

    ft_itoa done

--- libft.h
+++ libft.h
@@ -13,6 +13,7 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
+# include <limits.h>
 # include <unistd.h>
 # include <stdlib.h>
 
@@ -73,5 +74,7 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 
 char	**ft_split(char const *s, char c);
+
+char	*ft_itoa(int n);
 
 #endif
