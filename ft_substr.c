8aa3bea7fbcb68bfd76ba955f77e196fb395770e
Nabil080 <nbellila@student.42.fr>
2024-05-20 18:07:36 +0200

    fix substr alloc trop de byte (alloc tjrs len meme quand len plus grand que strlen - start

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 16:49:52 by nbellila          #+#    #+#             */
-/*   Updated: 2024/05/18 19:56:35 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/05/20 18:06:57 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,24 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	i;
+	size_t	s_len;
 	char	*str;
 
-	if ((size_t)start >= ft_strlen(s))
+	s_len = ft_strlen(s);
+	if ((size_t)start >= s_len)
 	{
 		str = malloc(1);
 		*str = 0;
 		return (str);
 	}
-	str = malloc((len + 1) * sizeof(char));
+	if (len < s_len - start + 1)
+		str = malloc((len + 1) * sizeof(char));
+	else
+		str = malloc((s_len - start + 1) * sizeof(char));
 	if (!str)
 		return (NULL);
 	i = 0;
-	while (i < len)
+	while (s[start + i] && i < len)
 	{
 		str[i] = s[start + i];
 		i++;
