d502fa2f1986ab0212e52b1b6ad65d1b82a3b6b7
Nabil080 <nbellila@student.42.fr>
2024-05-20 18:50:12 +0200

    lstclear done tested

--- ft_lstclear_bonus.c
+++ ft_lstclear_bonus.c
@@ -6,7 +6,7 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 14:06:37 by nbellila          #+#    #+#             */
-/*   Updated: 2024/05/20 14:10:56 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/05/20 18:49:57 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,15 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*next;
 
-	while ((*lst)->next)
+	if (!lst)
+		return ;
+	while(*lst)
 	{
 		next = (*lst)->next;
 		ft_lstdelone(*lst, del);
-		lst = &next;
+		*lst = next;
 	}
-	ft_lstdelone(*lst, del);
+	if (*lst)
+		ft_lstdelone(*lst, del);
 	lst = NULL;
 }
