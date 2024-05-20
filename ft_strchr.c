8aa2d967007c6a220d6d5a0342b6c8b0e4a8f3d8
Nabil080 <bellilanabil8@gmail.com>
2024-05-20 17:51:33 +0200

    Fix ft_strchr quand int pas ascii

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 14:31:52 by nbellila          #+#    #+#             */
-/*   Updated: 2024/05/18 19:02:51 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/05/20 17:51:09 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ char	*ft_strchr(const char *s, int c)
 {
 	size_t	i;
 
+	if (!ft_isascii(c))
+		return ((char *) s);
 	i = 0;
 	while (s[i])
 	{
@@ -27,3 +29,16 @@ char	*ft_strchr(const char *s, int c)
 		return ((char *) &s[i]);
 	return (NULL);
 }
+/*
+int main(void)
+{
+	char s[] = "teast";
+
+	printf("%s\n", ft_strchr(s, 't'));
+	printf("%s\n", ft_strchr(s, 'l'));
+	printf("%s\n", ft_strchr(s, 'z'));
+	printf("%s\n", ft_strchr(s, 0));
+	printf("%s\n", ft_strchr(s, 800));
+	return (0);
+}
+*/
