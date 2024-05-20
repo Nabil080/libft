1a9e1a8d7a7560098e24b0eb73b034e7d8e58978
Nabil080 <nbellila@student.42.fr>
2024-05-20 17:37:28 +0200

    fix lstaddback quand lst est vide

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -6,7 +6,7 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 14:07:57 by nbellila          #+#    #+#             */
-/*   Updated: 2024/05/20 14:07:59 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/05/20 17:35:33 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,41 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
+	if (!new)
+		return ;
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
 	while ((*lst)->next)
 		lst = &(*lst)->next;
 	(*lst)->next = new;
 }
+/*
+int	*ft_intdup(int n)
+{
+	int	*dup;
+
+	dup = malloc(sizeof(int));
+	*dup = n;
+	return (dup);
+}
+
+int main(void)
+{
+	size_t	i;
+	t_list	**lst;
+
+	lst = malloc(sizeof(t_list *));
+	*lst = ft_lstnew(ft_intdup(0));
+	i = 1;
+	while (i < 10)
+	{
+		ft_lstadd_back(lst, ft_lstnew(ft_intdup(i)));
+		i++;
+	}
+	printf("%i\n", *(int *)(*lst)->next->next->next->next->content);
+	return 0;
+}
+*/
