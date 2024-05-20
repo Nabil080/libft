7030afac9142128e6563ebba40f19ea9e1dfdfd9
Nabil080 <nbellila@student.42.fr>
2024-05-20 16:34:54 +0200

    lstnew tester

--- ft_lstnew_bonus.c
+++ ft_lstnew_bonus.c
@@ -6,7 +6,7 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 14:05:15 by nbellila          #+#    #+#             */
-/*   Updated: 2024/05/20 14:10:01 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/05/20 16:34:33 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,3 +23,32 @@ t_list	*ft_lstnew(void *content)
 	node->next = NULL;
 	return (node);
 }
+/*
+int	*ft_intdup(int n)
+{
+	int	*dup;
+
+	dup = malloc(sizeof(int));
+	*dup = n;
+	return (dup);
+}
+
+int main(void)
+{
+	size_t	i;
+	t_list	*lst;
+	t_list	*start;
+
+	lst = ft_lstnew(ft_intdup(0));
+	start = lst;
+	i = 1;
+	while (i < 10)
+	{
+		lst->next = ft_lstnew(ft_intdup(i));
+		lst = lst->next;
+		i++;
+	}
+	printf("%i\n", *(int *)start->content);
+	return 0;
+}
+*/
