1aa96f281a5adbcb2d638e551b9f5bfe2b529d78
Nabil080 <bellilanabil8@gmail.com>
2024-05-20 16:44:13 +0200

    lstadd front tester

--- ft_lstadd_front_bonus.c
+++ ft_lstadd_front_bonus.c
@@ -6,7 +6,7 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 14:04:20 by nbellila          #+#    #+#             */
-/*   Updated: 2024/05/20 14:04:22 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/05/20 16:43:48 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,38 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
+	if (!new)
+		return ;
 	new->next = *lst;
+	*lst = new;
 }
+/*
+int	*ft_intdup(int n)
+{
+	int	*dup;
+
+	dup = malloc(sizeof(int));
+	*dup = n;
+	return (dup);
+}
+
+int main(void)
+{
+	size_t	i;
+	t_list	**lst;
+	t_list	*new;
+
+	lst = malloc(sizeof(t_list *));
+	*lst = ft_lstnew(ft_intdup(0));
+	i = 1;
+	while (i < 10)
+	{
+		new = ft_lstnew(ft_intdup(i));
+		ft_lstadd_front(lst, new);
+		*lst = new;
+		i++;
+	}
+	printf("%i\n", *(int *)(*lst)->content);
+	return 0;
+}
+*/
