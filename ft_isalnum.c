c1fdb8be63262419ab16ee2ddad79ff2bcfa2154
Nabil080 <nbellila@student.42.fr>
2024-05-21 13:48:11 +0200

    isascii tester

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,7 +6,7 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 11:50:33 by nbellila          #+#    #+#             */
-/*   Updated: 2024/05/21 13:47:15 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/05/21 13:47:58 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@ int main(int argc, char const *argv[])
 {
 	if (argc != 2)
 		return (1);
-	printf("isalpha : %i\n", isalnum(*argv[1]));
-	printf("ft_isalpha : %i\n", ft_isalnum(*argv[1]));
+	printf("isalnum : %i\n", isalnum(*argv[1]));
+	printf("ft_isalnum : %i\n", ft_isalnum(*argv[1]));
 	return (0);
 }
 */
