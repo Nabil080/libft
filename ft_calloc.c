75037ef767ee34a3eb19ae3ac841fa294ecefc1f
Nabil080 <bellilanabil8@gmail.com>
2024-05-21 13:07:03 +0200

    calloc return null si area == 0 au lieu de ptr vide

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 14:31:06 by nbellila          #+#    #+#             */
-/*   Updated: 2024/05/20 17:58:44 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/05/21 13:06:22 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,11 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	void	*tab;
 	size_t	area;
 
-	if (size && nmemb > INT_MAX / size)
-		return (NULL);
 	if (nmemb == 0 || size == 0)
-		area = 1;
-	else
-		area = nmemb * size;
+		return (NULL);
+	if (nmemb > INT_MAX / size)
+		return (NULL);
+	area = nmemb * size;
 	tab = malloc(area);
 	if (!tab)
 		return (NULL);
