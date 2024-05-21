024a85c65a2cf88f6e2cb939bc246d29319a30a8
Nabil080 <nbellila@student.42.fr>
2024-05-21 13:08:25 +0200

    protection des malloc dans strndup de split

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 18:12:14 by nbellila          #+#    #+#             */
-/*   Updated: 2024/05/20 13:49:13 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/05/21 13:05:31 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ static	size_t	ft_countwords(char const *s, char c)
 	return (count);
 }
 
-char	*ft_strndup(const char *s, size_t n)
+static char	*ft_strndup(const char *s, size_t n)
 {
 	char	*dup;
 	size_t	i;
@@ -49,6 +49,26 @@ char	*ft_strndup(const char *s, size_t n)
 	return (dup);
 }
 
+static size_t	ft_successive_char(char const *s, char c)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] && s[i] == c)
+		i++;
+	return (i);
+}
+
+static size_t	ft_successive_not_char(char const *s, char c)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] && s[i] != c)
+		i++;
+	return (i);
+}
+
 char	**ft_split(char const *s, char c)
 {
 	char	**tab;
@@ -56,23 +76,43 @@ char	**ft_split(char const *s, char c)
 	size_t	start;
 	size_t	i;
 
-	tab = malloc ((ft_countwords(s, c) + 1) * sizeof(char *));
+	tab = malloc((ft_countwords(s, c) + 1) * sizeof(char *));
 	if (!tab)
 		return (NULL);
 	col = 0;
 	i = 0;
 	while (s[i])
 	{
-		while (s[i] && s[i] == c)
-			i++;
+		i += ft_successive_char(&s[i], c);
 		start = i;
-		while (s[i] && s[i] != c)
-			i++;
+		i += ft_successive_not_char(&s[i], c);
 		if (start == i)
 			break ;
 		tab[col] = ft_strndup(&s[start], i - start);
+		if (!tab[col])
+			return (NULL);
 		col++;
 	}
 	tab[col] = 0;
 	return (tab);
 }
+/*
+int	main(int argc, char const *argv[])
+{
+	char	**split;
+	size_t	i;
+
+	if (argc != 3)
+		return (1);
+	split = ft_split(argv[1], *argv[2]);
+	i = 0;
+	while (split[i])
+	{
+		printf("%s\n", split[i]);
+		free(split[i]);
+		i++;
+	}
+	free(split);
+	return (0);
+}
+*/
